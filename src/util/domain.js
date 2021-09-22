5500e6e2b881893dc14bb4f4c44a3da66e3acaf5
Philip <philipbdev@gmail.com>
2021-09-22 08:51:53 -0400

    Netlify

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,3 @@
 export default process.env.NODE_ENV === 'development'
   ? 'http://localhost:5000'
-  : process.env.NODE_ENV === 'production' &&
-    'https://forget-me-not-apothecary.herokuapp.com';
+  : process.env.NODE_ENV === 'production' && 'api.fmn-apothecary.store';
